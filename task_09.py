5af2b246466e4bb9923afe9846fd8e80b081a53e
VadimTalimonchik <tvadmins@gmail.com>
2023-02-12 18:15:19 +0300

    Задача 9 + варианты

--- task_09.py
+++ task_09.py
@@ -5,10 +5,26 @@
 # Input: 5
 # Output: 120
 
+# # v.1
+# factorial = 1
+# n = int(input('Введите число: '))
+# for i in range(1, n + 1):
+#     factorial *= i
+#     # print(i)
+#     # print(factorial)
+# print('Факториал числа', n, 'равен', factorial)
+
+# # v.2
+# num = int(input('Введите число: '))
+# factorial = 1
+# while num > 1:
+#     factorial *= num
+#     num -= 1
+# print(f'Факториал равен {factorial}')
+
+# v.3
+num = int(input('Введите число: '))
 factorial = 1
-n = int(input('Введите число: '))
-for i in range(1, n + 1):
+for i in range(1, num + 1):
     factorial *= i
-    # print(i)
-    # print(factorial)
-print('Факториал числа', n, 'равен', factorial)
+print(f'Факториал равен {factorial}')
